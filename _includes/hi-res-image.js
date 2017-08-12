8cf71a4818baaad2e3d9a7606cbe00284b2f4c79
Aaron Stacy <aaronstacy@google.com>
2017-08-12 17:51:52 -0500

    adjust image size based on size

---
+++ _includes/hi-res-image.js
@@ -0,0 +1,20 @@
+document.body.onload = function() {
+  [].slice.call(document.getElementsByTagName('img'), 0).forEach(function(img) {
+    var src = img.src;
+    var newImg = new Image();
+    var width = 760;
+
+    if (window.innerWidth < 760) {
+      return;
+    } else if (window.innerWidth < 1280) {
+      newImg.src = img.src.replace('-760', '-1280');
+    } else {
+      newImg.src = img.src.replace('-760', '');
+    }
+
+    newImg.onload = function() {
+      console.log('replacing ' + img.src + ' with ' + newImg.src);
+      img.src = newImg.src
+    };
+  });
+};
